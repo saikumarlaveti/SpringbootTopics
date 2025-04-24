b0a017729351743b54029cf1955bac2500cf1680
saikumar <saikumarlaveti5061@gmail.com>
2025-04-24 18:19:07 +0530

    spring scheduling completed

--- Concept_02_SpringBootScheduling/src/main/java/com/springScheduling/Service/Task1.java
+++ Concept_02_SpringBootScheduling/src/main/java/com/springScheduling/Service/Task1.java
@@ -8,13 +8,24 @@ import org.springframework.stereotype.Service;
 @Service("task1")
 public class Task1 {
 	
-	@Scheduled(initialDelay = 10000,fixedDelay=3000)
+	//@Scheduled(initialDelay = 10000,fixedDelay=3000)
+	//@Scheduled(initialDelay = 10000,fixedRate=3000)
+	@Scheduled(cron = "5 5 5 5 4 6 ")
 	public void doTask() {
 		System.out.println("Task1.doTask()" + new Date());
 	}
 
-	@Scheduled(fixedDelay=35000)
+	//@Scheduled(fixedDelay=35000)
 	public void greet() {
 		System.out.println("Hello Saikumar" );
 	}
 }
+
+/*
+ * First Star - seconds -->   "5 * * * * *"
+ * Second Star -  minutes --> "5 5 * * * *"
+ * Third Star -  05:05:05  --> "5 5 5 * * *"
+ * Fourth Star -  05:05:05 at 7 days --> "5 5 5 7 * *"
+ * Fifth Star - at 05:05:05 at 5 day at April month --> 5 5 5 5 4 *
+ * six Star - at 05:05:05 at 5 day at April month Saturday day --> 5 5 5 5 5 4 6 
+ */
